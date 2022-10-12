ea22ddff3bfcfb81bf77875feb1c645c8bd1bad0
Anbu V <60262202+Anbu8968@users.noreply.github.com>
2022-10-12 11:43:10 +0530

    Add files via upload
    uploaded the main file

---
+++ app.py
@@ -0,0 +1,20 @@
+from textblob import TextBlob
+import streamlit as st
+
+st.title("POSITIVE or NEGATIVE SENTENCE")
+label=st.subheader("SENTENCE")
+
+area=st.text_area(" ", value="", height=200, max_chars=None, key=None, help=None,placeholder="Hi Welcome Please Enter the Sentence....")
+# print(len(area))
+
+
+blob=TextBlob(area)
+if st.button("Predict"):
+    if len(area)>20:
+        if blob.sentiment.polarity >0:
+            # st.write(area)
+            st.subheader("Positive Sentence")
+        else:
+            st.subheader("Negative Sentence")
+    else:
+        st.subheader("Enter  Max 5 words")
